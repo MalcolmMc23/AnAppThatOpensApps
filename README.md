6269499e1277eff275f593667b032330c51d6b8a
Malcolm McDonald <malcolmemcdonald@gmail.com>
2022-10-28 09:41:08 -0700

    it now saves your file paths for future use
    there are two temp files for holding the path of your files
    temp = the "spotify' file path
    temp_R = the "reminder" file path
    
    note: they dont have to be spotify/reminder files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AnAppThatOpensApps
-this is just a little program im working that makes a GUI that can open other apps. Im making this to help myself learn how to make GUIs and get a better understanding of Java
+
+this is just a little program im working on that makes a GUI to open other apps. Im making this to help myself learn how to make GUIs and get a better understanding of Java.
+
+Note: you have to add .app to the end of your file when saving it.
--- myGUI/src/GUI.java
+++ myGUI/src/GUI.java
@@ -1,20 +1,19 @@
-
 import java.awt.Desktop;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-// import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-// import javax.swing.ActionListener;
 
 public class GUI implements ActionListener, java.awt.event.ActionListener {
 
@@ -31,7 +30,8 @@ public class GUI implements ActionListener, java.awt.event.ActionListener {
         frame = new JFrame();
         panel = new JPanel();
 
-        JButton chooseFile = new JButton(new AbstractAction("choose your Spotify file") {
+        JButton chooseFile = new JButton(new AbstractAction("choose your Spotify file") { // choose a file for the
+                                                                                          // spotify button
             @Override
             public void actionPerformed(ActionEvent e) {
                 clicks++;
@@ -45,10 +45,24 @@ public class GUI implements ActionListener, java.awt.event.ActionListener {
                 selFile = fc.getSelectedFile();
                 fileName_S = FileUtils.readFileToString(selFile);
                 System.out.println(fileName_S);
+
+                try {
+                    FileWriter writer = new FileWriter(
+                            "/Users/malcolm/Documents/MyProjects/AnAppThatOpensApps/myGUI/src/temp.txt");
+                    writer.write(fileName_S);
+                    writer.close();
+                    System.out.println("file was saved");
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+
             }
         });
 
-        JButton chooseReminder = new JButton(new AbstractAction("choose your Reminders file") {
+        JButton chooseReminder = new JButton(new AbstractAction("choose your Reminders file") { // choose a file path
+                                                                                                // for the reminders
+                                                                                                // button
             @Override
             public void actionPerformed(ActionEvent e) {
                 clicks++;
@@ -63,10 +77,21 @@ public class GUI implements ActionListener, java.awt.event.ActionListener {
                 fileName_R = FileUtils.readFileToString(selFile);
                 System.out.println(fileName_R);
 
+                try (
+                        FileWriter writer_R = new FileWriter(
+                                "/Users/malcolm/Documents/MyProjects/AnAppThatOpensApps/myGUI/src/temp_R.txt")) {
+                    writer_R.write(fileName_R);
+                    writer_R.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                System.out.println("file was saved");
+
             }
         });
 
-        JButton button = new JButton(new AbstractAction("open spotify") {
+        JButton button = new JButton(new AbstractAction("open spotify") { // runs selected spotify file path
             @Override
             public void actionPerformed(ActionEvent e) {
                 clicks++;
@@ -81,7 +106,7 @@ public class GUI implements ActionListener, java.awt.event.ActionListener {
                 }
             }
         });
-        JButton reminder_B = new JButton(new AbstractAction("open reminders") {
+        JButton reminder_B = new JButton(new AbstractAction("open reminders") { // runs selected reminders file path
             @Override
             public void actionPerformed(ActionEvent e) {
                 clicks++;
@@ -97,16 +122,46 @@ public class GUI implements ActionListener, java.awt.event.ActionListener {
             }
         });
 
+        JButton runAll = new JButton(new AbstractAction("Open All Apps") { // runs all selected file paths
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicks++;
+                label.setText("Number of clicks: " + clicks);
+
+                Path spotTemp = Path.of("/Users/malcolm/Documents/MyProjects/AnAppThatOpensApps/myGUI/src/temp.txt");
+                Path reTemp = Path.of("/Users/malcolm/Documents/MyProjects/AnAppThatOpensApps/myGUI/src/temp_R.txt");
+                try {
+                    String spotPath = Files.readString(spotTemp);
+                    String rePath = Files.readString(reTemp);
+
+                    Desktop d = Desktop.getDesktop();
+                    try {
+                        d.open(new File(spotPath));
+                        d.open(new File(rePath));
+                        System.out.println("all apps are open!");
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                } catch (IOException e2) {
+                    // TODO Auto-generated catch block
+                    e2.printStackTrace();
+                }
+            }
+        });
+
         label = new JLabel("number of clicks: " + clicks);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
         // panel.setLayout(new GridLayout(0, 1));
 
-        panel.add(chooseFile);
-        panel.add(button);
-        panel.add(chooseReminder);
+        panel.add(chooseFile); // opens a menue to select a spotify file path
+        panel.add(button); // runs the selected spotify button
 
-        panel.add(reminder_B);
+        panel.add(chooseReminder); // opens a menue to select a reminder file path
+        panel.add(reminder_B); // runs the selected reminder file
+
+        panel.add(runAll); // runs all selected files
 
         panel.add(label);
         frame.add(panel, BorderLayout.CENTER);
